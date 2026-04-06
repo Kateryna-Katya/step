957d428bb06e891c82d49d641d54cbd9942af56e
Kateryna <katyaukr2014@gmail.com>
2026-04-06 23:39:22 +0300

    add

--- src/js/form.js
+++ src/js/form.js
@@ -1,26 +1,54 @@
 const form = document.getElementById('contactForm');
-const btnText = form.querySelector('.btn-text');
-const loader = form.querySelector('.btn-loader');
-const message = form.querySelector('.form-message');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+if (form) {
+  const btnText = form.querySelector('.btn-text');
+  const loader = form.querySelector('.btn-loader');
+  const message = form.querySelector('.form-message');
 
-  // показуємо лоадер
-  btnText.classList.add('hidden');
-  loader.classList.remove('hidden');
-  message.classList.add('hidden');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  // імітація запиту (2 сек)
-  setTimeout(() => {
-    loader.classList.add('hidden');
-    btnText.classList.remove('hidden');
+    btnText.classList.add('hidden');
+    loader.classList.remove('hidden');
+    message.classList.add('hidden');
 
-    // fake success
-    message.textContent = 'Заявка успішно відправлена 🚀';
-    message.classList.remove('hidden');
-    message.classList.add('success');
+    setTimeout(() => {
+      loader.classList.add('hidden');
+      btnText.classList.remove('hidden');
 
-    form.reset();
-  }, 2000);
-});
+      message.textContent = 'Заявка успішно відправлена 🚀';
+      message.classList.remove('hidden');
+      message.classList.add('success');
+
+      form.reset();
+    }, 2000);
+  });
+}
+
+// ---- форма 2 ----
+const form2 = document.querySelector('.contact-form.purple');
+
+if (form2) {
+  const btnText2 = form2.querySelector('.btn-text');
+  const loader2 = form2.querySelector('.btn-loader');
+  const message2 = form2.querySelector('.form-message');
+
+  form2.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    btnText2.classList.add('hidden');
+    loader2.classList.remove('hidden');
+    message2.classList.add('hidden');
+
+    setTimeout(() => {
+      loader2.classList.add('hidden');
+      btnText2.classList.remove('hidden');
+
+      message2.textContent = 'Заявка відправлена 🚀';
+      message2.classList.remove('hidden');
+      message2.classList.add('success');
+
+      form2.reset();
+    }, 2000);
+  });
+}
